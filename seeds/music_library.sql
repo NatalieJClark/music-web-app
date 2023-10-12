07996281e612be0b48ed4c81e60451684bca0eb0
Natalie Clark <nj_clark@hotmail.co.uk>
2023-10-12 11:47:27 +0100

    Edit seed data

--- seeds/music_library.sql
+++ seeds/music_library.sql
@@ -7,13 +7,20 @@ CREATE SEQUENCE IF NOT EXISTS albums_id_seq;
 CREATE TABLE albums (
     id SERIAL PRIMARY KEY,
     title TEXT,
-    release_year, artist_id INT,
+    release_year INT,
     artist_id INT
 );
 
 -- Finally, we add any records that are needed for the tests to run
-INSERT INTO albums (title, release_year, artist_id) VALUES ('Invisible Cities', 'Italo Calvino');
-INSERT INTO albums (title, release_year, artist_id) VALUES ('The Man Who Was Thursday', 'GK Chesterton');
-INSERT INTO albums (title, release_year, artist_id) VALUES ('Bluets', 'Maggie Nelson');
-INSERT INTO albums (title, release_year, artist_id) VALUES ('No Place on Earth', 'Christa Wolf');
-INSERT INTO albums (title, release_year, artist_id) VALUES ('Nevada', 'Imogen Binnie');
+INSERT INTO albums (title, release_year, artist_id) VALUES ('Doolittle', 1989, 1);
+INSERT INTO albums (title, release_year, artist_id) VALUES ('Surfer Rosa', 1988, 1);
+INSERT INTO albums (title, release_year, artist_id) VALUES ('Waterloo', 1974, 2);
+INSERT INTO albums (title, release_year, artist_id) VALUES ('Super Trouper', 1980, 2);
+INSERT INTO albums (title, release_year, artist_id) VALUES ('Bossanova', 1990, 1);
+INSERT INTO albums (title, release_year, artist_id) VALUES ('Lover', 2019, 3);
+INSERT INTO albums (title, release_year, artist_id) VALUES ('Folklore', 2020, 3);
+INSERT INTO albums (title, release_year, artist_id) VALUES ('I Put a Spell on You', 1965, 4);
+INSERT INTO albums (title, release_year, artist_id) VALUES ('Baltimore', 1978, 4);
+INSERT INTO albums (title, release_year, artist_id) VALUES ('Here Comes the Sun', 1971, 4);
+INSERT INTO albums (title, release_year, artist_id) VALUES ('Fodder on My Wings', 1982, 4);
+INSERT INTO albums (title, release_year, artist_id) VALUES ('Ring Ring', 1973, 2);
